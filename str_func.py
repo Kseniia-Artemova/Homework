55e832fb4aedda6d030abd8562aed9edff45fc4a
Ksu <kseniyaartemova@yandex.ru>
2023-04-27 02:33:08 +0200

    added function str_title

--- str_func.py
+++ str_func.py
@@ -1,4 +1,9 @@
 def str_func(input_string):
     return input_string.upper()
 
+
+def str_title(input_string):
+    return input_string.title()
+
 print(str_func("wow"))
+print(str_title("hello world"))
